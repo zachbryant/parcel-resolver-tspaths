7adf51ea0420538e17bcdd194b14458c63ea2238
Zach Bryant <zdb282@gmail.com>
2021-03-05 03:38:03 -0800

    test app

--- test/testapp/src/index.ts
+++ test/testapp/src/index.ts
@@ -1,7 +1,7 @@
-//import pinata from 'url:@test/nested/pinata';
-
 import { greeting2 } from '@test/';
 import { greeting3 } from '@test/nested/index';
+
+//import pinata from '@test/nested/pinata.svg';
 
 console.log(greeting2);
 console.log(greeting3);
