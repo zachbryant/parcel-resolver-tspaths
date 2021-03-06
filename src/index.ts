7b370ed59e458962aa2b7887730d1c4c9d3ec279
Zach Bryant <zdb282@gmail.com>
2021-03-05 18:32:49 -0800

    removed index var

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-const path = require( 'path' );
-const fs = require( 'fs' );
+const path = require('path');
+const fs = require('fs');
 
 import { Resolver } from '@parcel/plugin';
 import { loadConfig } from '@parcel/utils';
@@ -12,28 +12,28 @@ import {
 type PathMapType = Map<string, string | Array<string>>;
 
 export default new Resolver({
-	async resolve( { filePath, dependency, options, logger } ) {
+	async resolve({ filePath, dependency, options, logger }) {
 		checkWebpackSpecificImportSyntax(dependency);
 		const pathsMap: PathMapType = await load(dependency.resolveFrom, options.inputFS, logger);
-		const isTypescript = dependency.resolveFrom.match( /\.tsx?$/g );
+		const isTypescript = dependency.resolveFrom.match(/\.tsx?$/g);
 		return {
 			filePath: isTypescript ? attemptResolve(filePath, pathsMap, logger) : undefined,
 		};
 	},
 });
 
-function attemptResolve( from: string, pathsMap: PathMapType, logger ) {
-	if ( from in pathsMap ) return pathsMap.get( from );
+function attemptResolve(from: string, pathsMap: PathMapType, logger) {
+	if (from in pathsMap) return pathsMap.get(from);
 
-	for ( let alias of Object.keys( pathsMap ) ) {
-		const aliasRegex = new RegExp( `^${alias.replace( "*", ".*" )}$`, 'g' )
-		if ( from.match( aliasRegex ) ) {
-			let value = pathsMap[ alias ];
-			switch ( value.constructor ) {
+	for (let alias of Object.keys(pathsMap)) {
+		const aliasRegex = new RegExp(`^${alias.replace('*', '.*')}$`, 'g');
+		if (from.match(aliasRegex)) {
+			let value = pathsMap[alias];
+			switch (value.constructor) {
 				case String:
 					return value;
 				case Array:
-					return attemptResolveArray( from, alias, value );
+					return attemptResolveArray(from, alias, value);
 			}
 		}
 	}
@@ -44,27 +44,27 @@ function attemptResolve( from: string, pathsMap: PathMapType, logger ) {
 // TODO support resource loaders like 'url:@alias/my.svg'
 /** Attempt to resolve any path associated with the alias to a file or directory index */
 function attemptResolveArray(from: string, alias: string, realPaths: Array<string>): string {
-	for ( let option of realPaths ) {
-		let unaliasedFrom = from.replace( trimStar(alias), trimStar( option ) );
-		let absolutePath = path.resolve( unaliasedFrom );
+	for (let option of realPaths) {
+		let unaliasedFrom = from.replace(trimStar(alias), trimStar(option));
+		let absolutePath = path.resolve(unaliasedFrom);
 
-		let fileExists = fs.existsSync( absolutePath );
-		if ( !fileExists ) {	// could be missing extension
-			const basename = path.basename( absolutePath );
-			const dirPath = path.dirname( absolutePath );
-			absolutePath = findFileInDirectory( dirPath, basename );
-			if ( !absolutePath ) absolutePath = findFileInDirectoryUnknownExt( dirPath, basename );
+		let fileExists = fs.existsSync(absolutePath);
+		if (!fileExists) {
+			// could be missing extension
+			const basename = path.basename(absolutePath);
+			const dirPath = path.dirname(absolutePath);
+			absolutePath = findFileInDirectory(dirPath, basename);
+			if (!absolutePath) absolutePath = findFileInDirectoryUnknownExt(dirPath, basename);
 		}
 
-		fileExists = fs.existsSync( absolutePath );
-		if ( fileExists ) {
-			let stats = fs.statSync( absolutePath );
-			if ( stats.isDirectory() ) {
-				let index = findFileInDirectory( absolutePath );
-				if ( !index ) continue;	// try another option, don't terminate early
-				absolutePath = `${ trimSlash( absolutePath ) }${ path.sep }${ path.basename( index ) }`
+		fileExists = fs.existsSync(absolutePath);
+		if (fileExists) {
+			let stats = fs.statSync(absolutePath);
+			if (stats.isDirectory()) {
+				absolutePath = findFileInDirectory(absolutePath);
+				if (!absolutePath) continue; // try another option, don't terminate early
 			}
-			return path.relative('.', absolutePath);	// parcel expects path from the project root
+			return path.relative('.', absolutePath); // parcel expects path from the project root
 		}
 	}
 	return undefined;
@@ -73,7 +73,7 @@ function attemptResolveArray(from: string, alias: string, realPaths: Array<strin
 async function load(resolveFrom: string, inputFS, logger): Promise<PathMapType> {
 	let result: PathMapType = await loadTsPaths(resolveFrom, inputFS, logger);
 	// TODO automatic tspath generation
-	logger.verbose({ message: `Typescript paths loaded: ${JSON.stringify(result)}` })
+	logger.verbose({ message: `Typescript paths loaded: ${JSON.stringify(result)}` });
 	return result;
 }
 
@@ -82,13 +82,13 @@ async function loadTsPaths(resolveFrom: string, inputFS, logger): Promise<PathMa
 	let result = await loadConfig(inputFS, resolveFrom, ['tsconfig.json']);
 	let config = result.config;
 
-	let baseUrl = config[ 'baseUrl' ] ?? 'src';
+	let baseUrl = config['baseUrl'] ?? 'src';
 
-	let tsPathsObject = config?.[ 'compilerOptions' ]?.[ 'paths' ] ?? {};
+	let tsPathsObject = config?.['compilerOptions']?.['paths'] ?? {};
 	let tsPathsMap = new Map<string, string | Array<string>>();
 
 	// Prepare map entries with baseUrl
-	for ( let [ key, value ] of Object.entries( tsPathsObject ) ) {
+	for (let [key, value] of Object.entries(tsPathsObject)) {
 		switch (value.constructor) {
 			case String:
 				tsPathsMap[key] = `${baseUrl}${path.sep}${value}`;
